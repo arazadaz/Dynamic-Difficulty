7398e96afa26ecc582b29195493d9c624673ef55
Arazadaz <Arazechaz@gmail.com>
2024-02-03 22:49:07 -0600

    Auto assignment of default origin type with ability to omit it. Added basic Origin registration method.

--- src/main/java/com/arazadaz/dd/api/OriginManager.java
+++ src/main/java/com/arazadaz/dd/api/OriginManager.java
@@ -13,13 +13,40 @@ public class OriginManager {
 
     private static HashMap<String, ArrayList<Origin>> originMap; //Mapping of origins to each world/all;
 
-    public static void addOrigin(Vec3 pos, String[] formulas, String[] types, double range, boolean noCalculationBound){
 
-        addOrigin(pos, formulas, types, range, noCalculationBound, "all");
+    public static void addOrigin(Vec3 pos, String[] types){ //basic, less control
+
+        double range = 0; //Will get global value from config after it's setup
+        String[] formulas = null; //will get global formulas from config after it's setup
+
+        addOrigin(pos, formulas, types, range, false, false, "all");
+
+    }
+    public static void addOrigin(Vec3 pos, String[] formulas, String[] types, double range, boolean noCalculationBound,  boolean omitDefaultType){ //Advanced, more control
+
+        addOrigin(pos, formulas, types, range, noCalculationBound, omitDefaultType, "all");
 
     }
 
-    public static void addOrigin(Vec3 pos, String[] formulas, String types[], double range, boolean noCalculationBound, String world){ //Optional assignment of origin to a specific world
+    public static void addOrigin(Vec3 pos, String[] formulas, String types[], double range, boolean noCalculationBound,  boolean omitDefaultType, String world){ //Optional assignment of origin to a specific world
+
+        String[] newTypes;
+
+        if(omitDefaultType){ //No modification
+
+            newTypes = types;
+
+        }else{ //Add default type to origin
+
+            newTypes = new String[types.length+1];
+
+            for(int i = 0; i<types.length; i++){
+                newTypes[i] = types[i];
+            }
+
+            newTypes[types.length] = "default";
+
+        }
 
         ArrayList<Origin> originsList;
 
@@ -29,7 +56,7 @@ public class OriginManager {
             originsList = new ArrayList<Origin>();
         }
 
-        Origin newOrigin = new Origin(pos, formulas, types, range, noCalculationBound);
+        Origin newOrigin = new Origin(pos, formulas, newTypes, range, noCalculationBound);
         originsList.add(newOrigin);
         originMap.put(world, originsList);
     }
