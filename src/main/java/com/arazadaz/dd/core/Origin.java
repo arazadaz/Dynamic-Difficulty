2ae0eee989f490b9bdf841b3a3d83022b0f7d9f6
Arazadaz <Arazechaz@gmail.com>
2024-02-04 19:53:52 -0600

    Some refactoring and implementation of isOriginType

--- src/main/java/com/arazadaz/dd/core/Origin.java
+++ src/main/java/com/arazadaz/dd/core/Origin.java
@@ -1,5 +1,6 @@
 package com.arazadaz.dd.core;
 
+import com.arazadaz.dd.api.Modes.*;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.phys.Vec3;
 
@@ -7,29 +8,30 @@ public class Origin {
 
     Vec3 pos;
     String[] formulas; //(for calculating from it(one for square and one for circle maybe), can be specified or uses a default formula)
-    String[] types;
+    String[] tags;
     double range; //How far out formula takes into consideration. Can be specified or uses a default global range
     boolean noCalculationBound; //Allows for origin to calculate past 1 or 100%
 
-    public Origin(Vec3 pos, String[] formulas, String[] types, double range, boolean noCalculationBound){
+    public Origin(Vec3 pos, String[] formulas, String[] tags, double range, boolean noCalculationBound){
 
         this.pos = pos;
         this.formulas = formulas;
-        this.types = types;
+        this.tags = tags;
         this.range = range;
         this.noCalculationBound = noCalculationBound;
 
     }
 
-    public double getDifficultyHere(LivingEntity entity){
+    public double getDifficultyHere(LivingEntity entity, DifficultyType type, RadiusMode rMode){
 
         Vec3 pos = entity.position();
 
-        return getDifficultyHere(pos);
+        return getDifficultyHere(pos, type, rMode);
 
     }
 
-    public double getDifficultyHere(Vec3 pos){
+    //Bulk of logic goes here
+    public double getDifficultyHere(Vec3 pos, DifficultyType type, RadiusMode mode){
 
         return 0;
 
@@ -39,7 +41,13 @@ public class Origin {
         return isOriginType("spawn");
     }
 
-    public boolean isOriginType(String type){
+    public boolean isOriginType(String tag){
+
+        for(String currentTag : tags){
+            if(currentTag.equals(tag)){
+                return true;
+            }
+        }
 
         return false;
 
